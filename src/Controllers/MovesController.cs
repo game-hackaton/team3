ba964b2cf8886a539d84f444b502098f0dbcd567
Crazy1beatch <zhora.shevelev196@mail.ru>
2024-03-30 15:14:33 +0500

    fix

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -15,13 +15,13 @@ public class MovesController : Controller
     [HttpPost]
     public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInput)
     {
-        if (userInput.KeyPressed == 37)
+        if (userInput.KeyPressed == 37 && game.Cells[^1].Pos.X - 1 >= 0)
             game.Cells[^1].Pos.X -= 1;
-        else if (userInput.KeyPressed == 39)
+        else if (userInput.KeyPressed == 39&& game.Cells[^1].Pos.X + 1 < 4)
             game.Cells[^1].Pos.X += 1;
-        else if (userInput.KeyPressed == 38)
+        else if (userInput.KeyPressed == 38&& game.Cells[^1].Pos.Y - 1 >= 0)
             game.Cells[^1].Pos.Y -= 1;
-        else if (userInput.KeyPressed == 40)
+        else if (userInput.KeyPressed == 40&& game.Cells[^1].Pos.Y + 1 < 4)
             game.Cells[^1].Pos.Y += 1;
         //else if (userInput.ClickedPos != null)
         //    game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;

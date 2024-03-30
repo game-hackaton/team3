06504e8d7144c2fdf8d3eeb0454ac47a5b54b172
Crazy1beatch <zhora.shevelev196@mail.ru>
2024-03-30 15:26:50 +0500

    fix movement

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -13,16 +13,32 @@ public class MovesController : Controller
     public static GameDto game = TestData.AGameDto(vector);
 
     [HttpPost]
-    public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInput)
+    public IActionResult Moves(Guid gameId, [FromBody] UserInputDto userInput)
     {
-        if (userInput.KeyPressed == 37 && game.Cells[^1].Pos.X - 1 >= 0)
-            game.Cells[^1].Pos.X -= 1;
-        else if (userInput.KeyPressed == 39&& game.Cells[^1].Pos.X + 1 < 4)
-            game.Cells[^1].Pos.X += 1;
-        else if (userInput.KeyPressed == 38&& game.Cells[^1].Pos.Y - 1 >= 0)
-            game.Cells[^1].Pos.Y -= 1;
-        else if (userInput.KeyPressed == 40&& game.Cells[^1].Pos.Y + 1 < 4)
-            game.Cells[^1].Pos.Y += 1;
+        while (true)
+        {
+            if (userInput.KeyPressed == 37 && game.Cells[^1].Pos.X - 1 >= 0)
+            {
+                game.Cells[^1].Pos.X -= 1;
+            }
+            else if (userInput.KeyPressed == 39 && game.Cells[^1].Pos.X + 1 < 4)
+            {
+                game.Cells[^1].Pos.X += 1;
+            }
+            else if (userInput.KeyPressed == 38 && game.Cells[^1].Pos.Y - 1 >= 0)
+            {
+                game.Cells[^1].Pos.Y -= 1;
+            }
+            else if (userInput.KeyPressed == 40 && game.Cells[^1].Pos.Y + 1 < 4)
+            {
+                game.Cells[^1].Pos.Y += 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
         //else if (userInput.ClickedPos != null)
         //    game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;
         return Ok(game);

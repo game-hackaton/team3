7e4346a4861407c7d9f012b8992d914a7786495d
rikgo <rikgogs@gmail.com>
2024-03-30 14:50:48 +0500

    simple move realization

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -9,12 +9,22 @@ namespace thegame.Controllers;
 [Route("api/games/{gameId}/moves")]
 public class MovesController : Controller
 {
+    public static VectorDto vector = new VectorDto { X = 1, Y = 1 };
+    public GameDto game = TestData.AGameDto(vector);
+
     [HttpPost]
     public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInput)
     {
-        var game = TestData.AGameDto(userInput.ClickedPos ?? new VectorDto {X = 1, Y = 1});
-        if (userInput.ClickedPos != null)
-            game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;
+        if (userInput.KeyPressed == 37)
+            game.Cells.LastOrDefault().Pos.X -= 1;
+        else if (userInput.KeyPressed == 39)
+            game.Cells.LastOrDefault().Pos.X += 1;
+        else if (userInput.KeyPressed == 38)
+            game.Cells.LastOrDefault().Pos.Y -= 1;
+        else if (userInput.KeyPressed == 40)
+            game.Cells.LastOrDefault().Pos.Y += 1;
+        //else if (userInput.ClickedPos != null)
+        //    game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;
         return Ok(game);
     }
 }
